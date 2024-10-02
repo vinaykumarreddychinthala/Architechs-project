90b76d223e31b3ce28a451623194ed673080fb85
vinaykumarreddychinthala <158133333+vinaykumarreddychinthala@users.noreply.github.com>
2024-10-02 16:12:03 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,82 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'lahsecoengg.wordpress.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'as1.ftcdn.net',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.promoteyourschool.co.uk',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'dynamic-media-cdn.tripadvisor.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.cnet.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.highspeedtraining.co.uk',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'cdn.iconfinder.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'media.istockphoto.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.pexels.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'i.pinimg.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.crazyvegankitchen.com',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'ariso.blob.core.windows.net',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 'cdn.uengage.io',
+            port: '',
+          },
+          {
+            protocol: 'https',
+            hostname: 't3.ftcdn.net',
+            port: '',
+          },
+
+
+
+        ],
+      },
+};
+
+export default nextConfig;
